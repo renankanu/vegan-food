c2608ce61983f97a7a31bb2f7949d9217b7a40eb
RenanKanu <renankanu@gmail.com>
2020-06-11 23:29:16 -0300

    Create Signup Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:vegan_food/screens/login/signup.dart';
 import 'package:vegan_food/screens/login/welcome.dart';
 
 void main() => runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Welcome(),
+    home: Signup(),
   )
 );
