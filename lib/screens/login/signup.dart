899b2c90dbb217f3b9ec23356880643b2eab5771
RenanKanu <renankanu@gmail.com>
2020-06-12 00:16:10 -0300

    Refactor signup

--- lib/screens/login/signup.dart
+++ lib/screens/login/signup.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:vegan_food/screens/login/widgets/custom_imput.dart';
 
 class Signup extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,8 +32,10 @@ class Signup extends StatelessWidget {
                   children: <Widget>[
                     Text(
                       "Registrar",
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold, fontFamily: 'Montserrat'),
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat'),
                     ),
                     SizedBox(
                       height: 20,
@@ -45,11 +46,42 @@ class Signup extends StatelessWidget {
                   padding: EdgeInsets.symmetric(horizontal: 40),
                   child: Column(
                     children: <Widget>[
-                      CustomInput(label: "Nome",),
-                      CustomInput(label: "Email",),
-                      CustomInput(label: "Senha", obscureText: true,),
-                      CustomInput(label: "Repetir Senha", obscureText: true,),
+                      CustomInput(
+                        label: "Nome",
+                      ),
+                      CustomInput(
+                        label: "Email",
+                      ),
+                      CustomInput(
+                        label: "Senha",
+                        obscureText: true,
+                      ),
+                      CustomInput(
+                        label: "Repetir Senha",
+                        obscureText: true,
+                      ),
                     ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {},
+                      color: Colors.green,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Text(
+                        "Enviar",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            fontFamily: 'Montserrat',
+                            color: Colors.white),
+                      ),
+                    ),
                   ),
                 )
               ],
