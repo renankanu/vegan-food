bbc07e16555b8a4447ffdfee50c4b30137dca3cc
RenanKanu <renankanu@gmail.com>
2020-06-13 17:17:30 -0300

    Init integration with i18n

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:vegan_food/screens/login/welcome.dart';
 
 void main() => runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
+    localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate
+    ],
+    supportedLocales: [
+      const Locale('pt', 'BR'),
+      const Locale('en', 'US'),
+    ],
+    locale: Locale('pt'),
     theme: ThemeData(fontFamily: 'Montserrat'),
     home: Welcome(),
   )
