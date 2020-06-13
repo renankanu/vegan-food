34766708ecc76f948bff64697d5345d93ae053ca
RenanKanu <renankanu@gmail.com>
2020-06-13 14:19:24 -0300

    set custom font in themedata

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:vegan_food/screens/login/welcome.dart';
 void main() => runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: 'Montserrat'),
     home: Welcome(),
   )
 );
